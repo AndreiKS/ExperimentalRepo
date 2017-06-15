56985e6f8d84b32be58f493defb6dbd5bfa44227
Andrei Kukharenko <carbon_kas@mail.ru>
2017-06-15 12:11:21 +0300

    *

--- ConsoleExcelReader/ConsoleExcelReader/Program.cs
+++ ConsoleExcelReader/ConsoleExcelReader/Program.cs
@@ -6,12 +6,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//repo
+
 namespace ConsoleExcelReader
 {
 
     class Program
     {
-        private static char[] arr ;
 
         static void Main(string[] args)
         {
@@ -24,41 +25,37 @@ namespace ConsoleExcelReader
             using (var fs = File.OpenRead(@"E:\118.csv"))
             using (var reader = new StreamReader(fs))
             {
-                char[] arr = new char[] { ';' };
+                char[] split = new char[] { ';' };
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
-                    var values = line.Split( arr, 5);
+                    var values = line.Split(split);
 
                     column1.Add(values[0]);
                     column2.Add(values[1]);
                     if (values.Length > 2)
-                    { 
-                    column3.Add(values[2]);
-                    column4.Add(values[3]);
+                    {
+                        column3.Add(values[2]);
+                        column4.Add(values[3]);
                     }
                 }
             }
-            
-            /*Console.WriteLine(column1[3]);
-            Console.WriteLine(column2[3]);
-            Console.WriteLine(column3[3]);
-            Console.WriteLine(column4[3]);
-            */
-            //StringBuilder s = new StringBuilder();
+
+            StringBuilder builder = new StringBuilder();
 
             List<string> dataAndTime = new List<string>();
 
             for (int i = 0; i < column2.Count; i++)
             {
-                dataAndTime[i] = column1[i] + " " + column2[i];
+                dataAndTime.Add(column1[i] + " " + column2[i]);
             }
 
 
 
-            string a = column1[3]+" " + column2[3];
+            string a = column1[3] + " " + column2[3];
             DateTime dt1 = DateTime.Parse(a);
-            DateTime dt2 = DateTime.Parse(dataAndTime[2]);
+            DateTime dt2;
+            DateTime.TryParse(dataAndTime[2], out dt2);
 
             //var smth = column1.Where(x=>x.)
 

ba156ef99c7c58d00dc14d5be951599cd1b4edb4
Andrei Kukharenko <carbon_kas@mail.ru>
2017-06-26 21:10:46 +0300

    + added ExcelReader, OpenXML SDK, some changes

---
+++ ConsoleExcelReader/ConsoleExcelReader/ExcelReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using S = DocumentFormat.OpenXml.Spreadsheet.Sheets;
+using E = DocumentFormat.OpenXml.OpenXmlElement;
+using A = DocumentFormat.OpenXml.OpenXmlAttribute;
+
+namespace ConsoleExcelReader
+{
+    public class ExcelReader
+    {
+
+        public static void GetSheetInfo(string fileName, ref string firstlist)
+        {
+            // Open file as read-only.
+            using (SpreadsheetDocument mySpreadsheet = SpreadsheetDocument.Open(fileName, false))
+            {
+                S sheets = mySpreadsheet.WorkbookPart.Workbook.Sheets;
+
+                // For each sheet, display the sheet information.
+                foreach (E sheet in sheets)
+                {
+                    foreach (A attr in sheet.GetAttributes())
+                    {
+                        Console.WriteLine("{0}: {1}", attr.LocalName, attr.Value);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
--- ConsoleExcelReader/ConsoleExcelReader/Program.cs
+++ ConsoleExcelReader/ConsoleExcelReader/Program.cs
@@ -5,21 +5,16 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using static ConsoleExcelReader.Helpers;
+using static System.Console;
 //repo
 // read .csv
 
 namespace ConsoleExcelReader
 {
     
-
     class Program
     {
-        public Program()
-        {
-
-        }
-
         static void Main(string[] args)
         {
             List<string> column1 = new List<string>();
@@ -27,11 +22,14 @@ namespace ConsoleExcelReader
             List<string> column3 = new List<string>();
             List<string> column4 = new List<string>();
             List<string> dataAndTimeString = new List<string>();
+
             int CTRatio = 40;
             string filename = @"E:\118.csv";
+            byte monthNumber = 3;
+
             Helpers.FileReader(filename, ref column1, ref column2, ref column3, ref column4);
             
-            List<DateTime>  dT = Helpers.DataFromTwoStrings(column1, column2, dataAndTimeString);
+            List<DateTime>  dT = DataFromTwoStrings(column1, column2, dataAndTimeString);
 
             double powerSum=0;
             double powerPeak = 0;
@@ -39,13 +37,16 @@ namespace ConsoleExcelReader
             double powerDay = 0;
             double maxPeakPower = 0;
             DateTime maxpeakPowerData = DateTime.MinValue;
-            DateTime requiredDate = new DateTime { };
+            DateTime selectedTime = new DateTime(2017, monthNumber, 2, 10,30,0);
+            selectedTime = selectedTime.AddMinutes(-30); // жэсточайшый костыль
+            double powerInSelectedTime = 0;
+            Zones z=Zones.Night;
 
             //var smth = column1.Where(x=>x.)
             for (int i = 0; i < column1.Count; i++)
             {
 
-                if (dT[i].Month == 3)
+                if (dT[i].Month == monthNumber)
                 {
                 double res = 0;
                 double.TryParse(column4[i].ToString(), out res);
@@ -55,7 +56,8 @@ namespace ConsoleExcelReader
                 if (dT[i].Hour >= 8 && dT[i].Hour <= 10)
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Zones z = Zones.Peak;
+                    z = Zones.Peak;
+                        if (dT[i] == selectedTime) powerInSelectedTime = res;
                     powerPeak += res;
                         if (res>=maxPeakPower)
                         {
@@ -67,35 +69,46 @@ namespace ConsoleExcelReader
                     | (dT[i].Hour >= 23 && dT[i].Hour <= 24))
                 {
                     Console.ForegroundColor = ConsoleColor.Cyan;
-                    Zones z = Zones.Night;
+                    z = Zones.Night;
                     powerNight += res;    
                 }
                 else
                 {
                         Console.ForegroundColor = ConsoleColor.White;
-                        Zones z = Zones.Night;
+                        z = Zones.Night;
                         powerDay += res;
                 }
 
                 Console.Write(dT[i]);
                 Console.WriteLine("         " +column3[i]+"    "+column4[i]);
-
+                
                 }
             }
             Console.ForegroundColor = ConsoleColor.White;
 
-            Console.WriteLine();
-            Console.WriteLine($"powerSum = {powerSum}");
-            double consumption = powerSum * CTRatio/2;
+            WriteLine();
+            WriteLine("месяц № " + monthNumber);
+            WriteLine($"powerSum = {powerSum}");
+            double consumption = Helpers.ConvertToRealValue (powerSum , CTRatio);
             Console.WriteLine($"consumption= { consumption}");
-            Console.WriteLine($"consumptionPeak= { powerPeak * CTRatio / 2} or {powerPeak/ powerSum * 100}%");
-            Console.WriteLine($"consumptionDay= { powerDay * CTRatio / 2} or {powerDay / powerSum * 100, 2}%");
-            Console.WriteLine($"consumptionNight= { powerNight * CTRatio/2} or {powerNight / powerSum * 100, 2}%");
+            Console.WriteLine($"consumptionPeak= { powerPeak * CTRatio / 2 } or {powerPeak/ powerSum * 100 }%");
+            Console.WriteLine($"consumptionDay= { powerDay * CTRatio / 2 } or {powerDay / powerSum * 100 }%");
+            Console.WriteLine($"consumptionNight= { powerNight * CTRatio/2 } or {powerNight / powerSum * 100 }%");
             Console.WriteLine();
-
+            maxpeakPowerData = maxpeakPowerData.AddMinutes(30); // приводим к концу диапазона, как принято
             Console.WriteLine($"maxPeakPower = {maxPeakPower*CTRatio} ");
             Console.WriteLine($"maxPeakPowerData = {maxpeakPowerData} ");
 
+            Console.WriteLine("выбранное время  " + selectedTime+ "   мощность  "+ powerInSelectedTime*CTRatio);
+            Console.ReadKey();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            string fName = @"E:\118 столовая до 06.06.2017.xlsx";
+
+            string firstlist="";
+
+            ExcelReader.GetSheetInfo(fName, ref firstlist);
             Console.ReadKey();
 
         }
